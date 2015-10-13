7180dc59f8701cda6b2ee526f478fb7b66b0be75
Jesus Garza <chamoysvoice@gmail.com>
2015-10-12 23:27:21 -0500

    added coppel card

--- peda.rb
+++ peda.rb
@@ -1,14 +1,16 @@
 # Mijito, tu que estudiaste programacion, programate una peda chida
+# PD: macho pecho peludo comienza en jueves y tiene credito coppel
 class Peda
 
-  def initialize n_pomos, n_amigos, time
+  def initialize n_pomos, n_amigos, time, coppel_card
     @n_pomos = n_pomos
     @n_amigos = n_amigos
+    @coppel_card = coppel_card
     @time = time
   end
   
   def armar
-    if @time.wday == 6 and @n_pomos > 0 and @n_amigos > 0 # wday = 6 => saturday
+    if @time.wday == 4 and (@n_pomos > 0 or @coppel_card) and @n_amigos > 0 # wday = 4 => thursday
       "ya se armo este pex"
     else
       "ya se chingo este pex"
@@ -17,5 +19,5 @@ class Peda
 
 end
 
-laPeda = Peda.new(10000, 5, Time.now) # depending on system time
+laPeda = Peda.new(10000, 5, Time.now, true) # depending on system time
 puts laPeda.armar
